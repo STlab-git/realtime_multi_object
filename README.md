7636029da195813394a206f1ffd1837de1883343
R-Fukatsu-prv <ryuoneryu@yahoo.co.jp>
2021-04-25 02:05:39 +0900

    made initial enviroment

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # realtime_multi_object
+## Environment
+Apache Maven 3.6.3 <br>
+Java 14.0.2
+
+## Overview
+An example of parallel running of objects
---
+++ src/main/java/RMO/Main.java
@@ -0,0 +1,18 @@
+package RMO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+
+public static void main(String[] arg) {
+		
+		final Logger logger = LoggerFactory.getLogger(Main.class);
+		
+		logger.info("start to read a config file");
+        //ReadConfig config = new ReadConfig();        
+        
+	}
+	
+}
